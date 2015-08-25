ea1de1633cef01c049ce98c98120e01011919a20
dtx12 <dtx1212@gmail.com>
2015-08-25 10:20:04 +0300

    [IW-3] Created basic structure for disassembler

---
+++ DIsassembler.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Resolver;
+
+namespace DIsassembler.Console
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                var disassembler = new Disassembler.Disassembler(new DebugResolver(false, Game.Ghosts), dialog.FileName);
+            }
+        }
+    }
+}
---
+++ Disassembler/Disassembler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Resolver;
+
+namespace Disassembler
+{
+    public class Disassembler
+    {
+        private readonly byte[] _buffer;
+        private readonly byte[] _bytecode;
+        private readonly BaseResolver _resolver;
+
+        /// <summary>
+        ///     Creates instance of disassembler
+        /// </summary>
+        /// <param name="resolver">Resolver</param>
+        /// <param name="path">path to target file</param>
+        public Disassembler(BaseResolver resolver, string fileName)
+        {
+            _resolver = resolver;
+            var path = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName));
+            var bytecodePath = path + ".bytecode";
+            var bufferPath = path + ".buffer";
+            if (!File.Exists(bytecodePath))
+            {
+                throw new ArgumentOutOfRangeException("File with script bytecode is not exists!");
+            }
+            if (!File.Exists(bufferPath))
+            {
+                throw new ArgumentOutOfRangeException("File with script buffer is not exists!");
+            }
+            _bytecode = File.ReadAllBytes(bytecodePath);
+            _buffer = File.ReadAllBytes(bufferPath);
+        }
+    }
+}
